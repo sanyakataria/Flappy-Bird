8e4987c6f3799d27cd78c62c65130563393ef64d
sanyakataria <52246974+sanyakataria@users.noreply.github.com>
2019-09-01 01:26:54 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -12,6 +12,18 @@ const DEGREE = Math.PI/180;
 const sprite = new Image();
 sprite.src = "img/sprite.png";
 
+//LOAD SOUNDS
+const SCORE_S = new Audio();
+SCORE_S.src = "audio/sfx_point.wav";
+const FLAP = new Audio();
+FLAP.src = "audio/sfx_flap.wav";
+const HIT = new Audio();
+HIT.src = "audio/sfx_hit.wav";
+const SWOOSHING = new Audio();
+SWOOSHING.src = "audio/sfx_swooshing.wav";
+const DIE = new Audio();
+DIE.src = "audio/sfx_die.wav";
+
 // GAME STATE
 const state = {
     current : 0,
@@ -20,15 +32,38 @@ const state = {
     over : 2
 }
 
+//START BUTTON COORD
+const startBtn = {
+    x : 120,
+    y : 263,
+    w : 83,
+    h : 29
+}
+
 // CONTROL THE GAME
 cvs.addEventListener("click", function(evt) {
     switch(state.current) {
-        case state.getReady: state.current = state.game;
-                             break;
-        case state.game: bird.flap();
-                         break;
-        case state.over: state.current = state.getReady;
-                         break;
+        case state.getReady: 
+            state.current = state.game;
+            SWOOSHING.play();
+            break;
+        case state.game: 
+            bird.flap();
+            FLAP.play();
+            break;
+        case state.over: 
+            let rect = cvs.getBoundingClientRect();
+            let clickX = evt.clientX - rect.left;
+            let clickY = evt.clientY - rect.top;
+
+            // checl if we click on the start button
+            if(clickX >= startBtn.x && clickX <= startBtn.x + startBtn.w && clickY >= startBtn.y && clickY <= startBtn.y + startBtn.h){
+                pipes.reset();
+                bird.speedReset();
+                score.reset();
+                state.current = state.getReady;
+            }
+            break;
     }
 })
 
@@ -90,6 +125,8 @@ const bird = {
     speed : 0,
     rotation : 0,
 
+    radius : 12,
+
     draw : function() {
         let bird = this.animation[this.frame];
 
@@ -126,6 +163,7 @@ const bird = {
                 this.y = cvs.height - fg.h - this.h/2;
                 if(state.current == state.game){
                     state.current = state.over;
+                    DIE.play();
                 }
             }
 
@@ -136,7 +174,9 @@ const bird = {
             }else
                 this.rotation = -25 * DEGREE;
         }
-
+    },
+    speedReset : function(){
+        this.speed=0;
     }
 }
 
@@ -172,22 +212,139 @@ const gameOver = {
     }
 }
 
+//PIPES
+const pipes = {
+    position : [],
+
+    top : {
+        sX : 553,
+        sY : 0
+    },
+    bottom : {
+        sX : 502,
+        sY : 0
+    },
+
+    w : 53,
+    h : 400,
+    gap : 85,
+    maxYPos : -150,
+    dx : 2,
+
+    draw : function(){
+        for(let i=0; i<this.position.length; i++){
+            let p = this.position[i];
+            // console.log(p)
+
+            let topYPos = p.y;
+            let bottomYPos = p.y + this.h + this.gap;
+
+            //top pipe
+            ctx.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos, this.w, this.h);
+           
+            //bottom pipe
+            ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos, this.w, this.h);
+            
+        }
+    },
+
+    update : function(){
+        if(state.current !== state.game) return;
+
+        if(frames%100 == 0){
+            this.position.push({
+                x : cvs.width,
+                y : this.maxYPos * (Math.random() + 1)
+            });
+            console.log(this.position)
+        }
+        for(let i=0; i<this.position.length; i++){
+            let p = this.position[i];
+
+            let bottomPipeYPos = p.y + this.h + this.gap;
+            // console.log(bottomPipeYPos)
+
+            //collison detection
+            //top pipe
+            if(bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > p.y && bird.y - bird.radius < p.y + this.h){
+                state.current = state.over;
+                HIT.play();
+            }
+            //bottom pipe
+            if(bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > bottomPipeYPos && bird.y - bird.radius < bottomPipeYPos + this.h){
+                state.current = state.over;
+                HIT.play();
+            }
+
+            //move the pipes to the left
+            p.x -= this.dx;
+            
+            // if the pipes go beyond the canvas, we delete them from the array
+            if(p.x + this.w <= 0){
+                this.position.shift();
+                score.value += 1;
+                SCORE_S.play();
+
+                score.best = Math.max(score.value, score.best)
+                localStorage.setItem("best", score.best)
+            }
+        }
+    },
+    
+    reset : function(){
+        this.position=[];
+    }
+}
+
+//SCORE
+const score = {
+    best : parseInt(localStorage.getItem("best")) || 0,
+    value : 0,
+
+    draw : function(){
+        ctx.fillStyle = "#FFF";
+        ctx.strokeStyle = "#000";
+
+        if(state.current == state.game){
+            ctx.lineWidth = 2;
+            ctx.font = "35px Teko";
+            ctx.fillText(this.value, cvs.width/2, 50);
+            ctx.strokeText(this.value, cvs.width/2, 50);
+        }else if(state.current == state.over){
+            //score value
+            ctx.font = "25px Teko";
+            ctx.fillText(this.value, 225, 186);
+            ctx.strokeText(this.value, 225, 186);
+            // best score
+            ctx.fillText(this.best, 225, 228);
+            ctx.strokeText(this.best, 225, 228);
+        }
+    },
+
+    reset : function(){
+        this.value = 0;
+    }
+}
+
 // DRAW
 function draw(){
     ctx.fillStyle = "#70c5ce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
 
     bg.draw();
+    pipes.draw();
     fg.draw();
     bird.draw();
     getReady.draw();
     gameOver.draw();
+    score.draw();
 }
 
 //UPDATE
 function update(){
     bird.update();
     fg.update();
+    pipes.update();
 }
 
 //LOOP
