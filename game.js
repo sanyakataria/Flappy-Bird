e2ae4bf2932c08ad37bd3e25f71b7fb856429dbd
sanyakataria <52246974+sanyakataria@users.noreply.github.com>
2020-10-29 21:10:30 +0530

    Update game.js

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ const DEGREE = Math.PI/180;
 
 // LOAD SPRITE IMAGE
 const sprite = new Image();
-sprite.src = "img/sprite.png";
+sprite.src = "sprite.png";
 
 //LOAD SOUNDS
 const SCORE_S = new Audio();
